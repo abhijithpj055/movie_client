f9c61becc3d67b9e30b9faa17674e8fb415547fa
Abhijith P J <apj42211@gmail.com>
2025-08-15 18:41:43 +0530

    front-end first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
- 
-
   return (
-    <>
-    <h1>movies</h1>
-    </>
+    <div>
+      <h1>movies</h1>
+    </div>
   )
 }
 
